f84b3fd2dfa85f729487b110e78040787a1f4dca
ssharma105 <161381647+ssharma105@users.noreply.github.com>
2024-02-28 13:36:00 -0500

    Update top.sv 3

--- src/top.sv
+++ src/top.sv
@@ -26,7 +26,7 @@ module top(input logic clk, input logic reset, input logic [31:0] cyc_cnt, outpu
    logic rst_n = ! reset;
 
    // Instantiate the Tiny Tapeout module.
-   tt_um_fpga_hdl_demo tt(.*);
+   tt_um_template tt(.*);
 
    assign passed = top.cyc_cnt > 60;
    assign failed = 1'b0;
@@ -43,7 +43,7 @@ endmodule
 // The Tiny Tapeout module
 // =======================
 
-module tt_um_fpga_hdl_demo (
+module tt_um_template (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     /*   // The FPGA is based on TinyTapeout 3 which has no bidirectional I/Os (vs. TT6 for the ASIC).
